35e1671209a9939928e5b900a1ebbcaa3fa44cce
OBAID <muhammadobaid103@gmail.com>
2024-10-11 10:24:42 +0500

    Add files via upload

---
+++ ICT_LAB/LAB_06/script.js
@@ -0,0 +1,8 @@
+<script>
+            var name = "OBAID";
+            console.log(name);
+            name ="obaid";
+            console.log(name);
+            var name = "MUHAMMAD"
+            console.log(name);
+        </script>
