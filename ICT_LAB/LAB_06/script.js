845f24277c19c5cf5d523169dac1b3721cb13c61
OBAID <muhammadobaid103@gmail.com>
2025-02-03 22:58:36 +0500

    Delete ICT_LAB directory

--- ICT_LAB/LAB_06/script.js
+++
@@ -1,8 +0,0 @@
-<script>
-            var name = "OBAID";
-            console.log(name);
-            name ="obaid";
-            console.log(name);
-            var name = "MUHAMMAD"
-            console.log(name);
-        </script>
