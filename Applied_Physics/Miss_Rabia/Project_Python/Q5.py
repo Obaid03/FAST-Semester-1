379c1122b7e11ea01dbd46d00c0d028b71b0dc43
OBAID <muhammadobaid103@gmail.com>
2025-02-03 22:06:50 +0500

    Delete Applied_Physics/Miss_Rabia/Project_Python directory

--- Applied_Physics/Miss_Rabia/Project_Python/Q5.py
+++
@@ -1,9 +0,0 @@
-import math as m 
-magr=7.3
-angle=m.radians(30)
-rx=magr*m.cos(angle)
-ry=magr*m.sin(angle)
-print("a)")
-print("X component of Vector R =", (rx))
-print("b)")
-print("Y component of Vector R =", (ry))
